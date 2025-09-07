6f0dcb69184667048e832875c465b5c9afdfec4b
git <karolayneamabile@gmail.com>
2025-09-07 12:26:50 -0300

    testando o watch tower2

--- docker-django/src/mydjango/urls.py
+++ docker-django/src/mydjango/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import url ,  include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+ 
 
 from django.urls import path, include 
 from django.http import HttpResponse
